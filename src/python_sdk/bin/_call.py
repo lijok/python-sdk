cc95fcb2e8d499c0a68c8b798a997e31468dd521
lijok <lijok@pm.me>
2023-04-29 11:00:41 +0100

    Improve robustness of sudo and binary full path resolution in bin.call

--- src/python_sdk/bin/_call.py
+++ src/python_sdk/bin/_call.py
@@ -3,23 +3,32 @@ import functools
 import logging
 import os
 import pathlib
+import shutil
 import subprocess
 import typing
 
-from python_sdk import utils
 
-
-@dataclasses.dataclass
-class BinaryNotInstalled(FileNotFoundError):
+class BinaryNotInstalled(Exception):
+    message: str
     binary: str
+
+    def __init__(self, binary: str, message: str = "Binary not installed.") -> None:
+        super().__init__(message, binary)
+        self.message = message
+        self.binary = binary
 
     def __str__(self) -> str:
         return self.binary
 
 
 class CalledProcessError(Exception):
-    def __init__(self, message: str, output: str, exit_code: int) -> None:
+    message: str
+    output: str
+    exit_code: int
+
+    def __init__(self, output: str, exit_code: int, message: str = "Called process failure.") -> None:
         super().__init__(message, output, exit_code)
+        self.message = message
         self.output = output
         self.exit_code = exit_code
 
@@ -42,15 +51,40 @@ def call(
         BinaryNotInstalled: binary not installed
     """
     env = environment_variables if environment_variables is not None else dict(os.environ)
+    arguments = [str(argument) for argument in args]
+
+    if args[0] == "arch":
+        # Caller supplied arch in args, instead of the force_arch argument. Let's pull it out.
+        force_arch = args[1]
+        arguments = list(args[2:])
+
+    if args[0].endswith("sudo"):
+        # Caller supplied sudo in args, instead of the sudo argument. Let's pull it out.
+        sudo = True
+        sudo_binary = args[0]
+        arguments = list(args[1:])
+
+    if sudo:
+        path_to_sudo = shutil.which(sudo_binary)
+        if not path_to_sudo:
+            raise BinaryNotInstalled(binary=sudo_binary)
+        sudo_full_path = pathlib.Path(path_to_sudo)
+        if not sudo_full_path.exists():
+            raise BinaryNotInstalled(binary=str(sudo_full_path))
+        if not os.access(sudo_full_path, os.EX_OK):
+            raise PermissionError(f"{sudo_full_path} is not executable.")
 
     binary: str = args[0]
-    arguments: typing.Any = [str(argument) for argument in args[1:]]
+    arguments = list(args[1:])
 
-    try:
-        binary_full_path = utils.which(binary)
-        sudo_full_path = utils.which(sudo_binary)
-    except FileNotFoundError as e:
-        raise BinaryNotInstalled(binary=e.filename) from e
+    path_to_binary = shutil.which(binary)
+    if not path_to_binary:
+        raise BinaryNotInstalled(binary=binary)
+    binary_full_path = pathlib.Path(path_to_binary)
+    if not binary_full_path.exists():
+        raise BinaryNotInstalled(binary=str(binary_full_path))
+    if not os.access(binary_full_path, os.EX_OK):
+        raise PermissionError(f"{binary_full_path} is not executable.")
 
     command: list[str | pathlib.Path] = [binary_full_path] + arguments
     if sudo:
@@ -77,6 +111,6 @@ def call(
     process.wait()
 
     if process.returncode:
-        raise CalledProcessError("Called process failure.", output=output, exit_code=process.returncode)
+        raise CalledProcessError(output=output, exit_code=process.returncode)
 
     return output.strip()
