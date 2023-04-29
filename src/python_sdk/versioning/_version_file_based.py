1a10fe79446b0b90a31ad0969a99f584d88ee088
lijok <lijok@pm.me>
2023-04-29 22:34:56 +0100

    Add debug log to versioning.version_file_based

--- src/python_sdk/versioning/_version_file_based.py
+++ src/python_sdk/versioning/_version_file_based.py
@@ -1,23 +1,12 @@
 import inspect
 import json
+import logging
 import os
 import pathlib
 
 
 # TODO: Add debug logs
 def version_file_based(version_file_name: str = "version") -> str:
-    # TODO: In manual testing, this appears to not be required. Will need to add automated tests.
-    # if hasattr(sys, "_MEIPASS"):  # Running as a PyInstaller frozen application
-    #     packaged_version_file = pathlib.Path(getattr(sys, "_MEIPASS")) / version_file_name
-    #     if not packaged_version_file.exists():
-    #         raise FileNotFoundError(
-    #             "Version lookup failed. "
-    #             "Version file not found. "
-    #             "We detected we're running in a PyInstaller frozen application. "
-    #             f"Expected to find version file at: {packaged_version_file}."
-    #         )
-    #     return packaged_version_file.read_text()
-
     calling_module_frame = inspect.stack()[1]
     calling_module_name = calling_module_frame[0]
     calling_module = inspect.getmodule(calling_module_name)
@@ -30,6 +19,8 @@ def version_file_based(version_file_name: str = "version") -> str:
     while True:
         currently_checking = current_dir / version_file_name
         checked.append(currently_checking)
+
+        logging.debug(f"Searching for version file. {currently_checking=}")
 
         if currently_checking.exists():
             return currently_checking.read_text().strip()
